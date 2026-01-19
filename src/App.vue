350082f101601a80c778a7e2b99980c93aaa4730
Matthew-FE-OJT <163794657+MatthewQxS@users.noreply.github.com>
2026-01-20 02:07:33 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@ import CursorLight from "./components/ui/CursorLight.vue";
 </script>
 
 <template>
-  <nav>Navbar here</nav>
-  <main style="background:red; min-height:200vh;">
-    <p style="color:white;">CONTENT TEST</p>
-  </main>
+  <router-view />
+  <!-- <CursorLight /> -->
 </template>
-
